6f5ceaca2145510b4c82061b57ccb1ac36615203
phat2005 <doctorgamer1156@gmail.com>
2026-05-23 19:14:10 +0700

    phế phề phê

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,10 +25,10 @@ android {
         applicationId = "com.example.clone_mxh"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode()
-        versionName = flutter.versionName()
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
